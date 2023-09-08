456fe1c373f5a008a78bf0f1863fa40945fcb381
Lucian <lucian.voju@viavisolutions.com>
2023-09-08 10:01:58 +0300

    create a basic route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import {HomeComponent} from '@home-page'
+
+export const appRoutes: Route[] = [
+    {
+        path:'',
+        component: HomeComponent,
+        pathMatch: 'full'
+    }
+];
